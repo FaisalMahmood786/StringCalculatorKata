0f21c3c8d60263d6a9a129ff7b7c382495555925
Faisal Mahmood <Faisal.Mahmood@evolutionmoney.co.uk>
2024-08-07 23:13:05 +0100

    Implement the Minimum Code (Green)

---
+++ StringCalculatorKata.Tests/StringCalculatorTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalculatorKata.Tests
+{
+    [TestFixture]
+    public class StringCalculatorTests
+    {
+        [Test]
+        public void Add_EmptyString_ReturnsZero()
+        {
+            var calculator = new StringCalculator();
+            var result = calculator.Add("");
+           Assert.AreEqual(0, result);
+        }
+    }
+}
+
